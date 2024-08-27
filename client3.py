59923defde574c5b3ccb883e0ebe966946fc463d
DrishyaShah <drishya1912@gmail.com>
2024-08-27 11:21:06 +0530

    task done

--- client3.py
+++ client3.py
@@ -1,59 +1,63 @@
-################################################################################
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a
-#  copy of this software and associated documentation files (the "Software"),
-#  to deal in the Software without restriction, including without limitation
-#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
-#  and/or sell copies of the Software, and to permit persons to whom the
-#  Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in
-#  all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#  DEALINGS IN THE SOFTWARE.
-
 import json
 import random
 import urllib.request
 
 # Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
+QUERY = "http://localhost:5000/query?id={}"
 
-# 500 server request
+# 500 server requests
 N = 500
 
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
-    return stock, bid_price, ask_price, price
+    if isinstance(quote, dict):
+        stock = quote.get('stock', '')
+        bid_price = float(quote.get('top_bid', {}).get('price', 0))
+        ask_price = float(quote.get('top_ask', {}).get('price', 0))
+        price = (bid_price + ask_price) / 2
+        return stock, bid_price, ask_price, price
+    else:
+        print("Unexpected quote format:", quote)
+        return None, None, None, None
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+    for _ in range(N):
+        try:
+            # Make the API request
+            response = urllib.request.urlopen(QUERY.format(random.random()))
+            data = response.read()
+            quotes = json.loads(data)
 
-        """ ----------- Update to get the ratio --------------- """
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            # Print the raw response for inspection
+            print("API Response:", quotes)
 
-        print("Ratio %s" % getRatio(price, price))
+            prices = {}
+            for quote in quotes:
+                stock, bid_price, ask_price, price = getDataPoint(quote)
+                if stock:
+                    prices[stock] = price
+                    print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+            # Calculate and print the ratio if both stocks are present
+            if "ABC" in prices and "DEF" in prices:
+                ratio = getRatio(prices["ABC"], prices["DEF"])
+                if ratio is not None:
+                    print("Ratio %s" % ratio)
+                else:
+                    print("Error: Cannot calculate ratio. Division by zero.")
+            else:
+                print("Missing price data for one or both stocks.")
+
+        except Exception as e:
+            print("Error during request or processing:", e)
